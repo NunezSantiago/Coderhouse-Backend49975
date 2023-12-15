0717084d37f8c0a6856ca90cf16323b671b6f6b1
santiago.nunez <nunezsantiago43@gmail.com>
2023-12-15 20:56:56 -0300

    Pre entrega 2 Final

--- Pre-entrega 2/src/public/js/productHandler.js
+++ Pre-entrega 2/src/public/js/productHandler.js
@@ -1,56 +1,20 @@
 const socket = io()
-//const cartManager = require('./dao/cartManager_MONGO.js')
-
-//let cm = new cartManager()
-
-
-
-// Swal.fire({
-//     title: "Ingrese un nombre",
-//     input: "number",
-//     text: "Nombre:",
-//     inputValidator: (value) => {
-//         return !value && "Por favor, ingrese su nombre."
-//     },
-//     allowOutsideClick: false
-// }).then(result => {
-//     let cartID = result.value
-
-//     let addToCart = document.getElementsByClassName('addToCart')
-
-//     let divProductos = document.getElementById("productos")
-    
-//     let divProductos = document.getElementById("productos")
-//     divProductos.innerHTML = `${products}`
-
-//     let productID
-// })
 
 async function addToCart(pid){
-    // let divProductos = document.getElementById("productos")
-    // divProductos.innerHTML = `${pid}`
-
+    
     let cartID = document.getElementById('cartID').value
 
-    // let divProductos = document.getElementById("productos")
-    // divProductos.innerHTML = `${cartID}`
-
     let URL = `/api/carts/${cartID}/product/${pid}`
-    // let divProductos = document.getElementById("productos")
-    // divProductos.innerHTML = `${URL}`
+    
 
     fetch(URL, {
-        method: 'PUT',
-        body: JSON.stringify({
-            "quantity": 5
-        })
+        method: 'PUT'
     })
 
 
 }
 
 socket.on("connection", async (data)=>{
-    //console.log('hola')
 })
 
 socket.on("new-product", data=>{
